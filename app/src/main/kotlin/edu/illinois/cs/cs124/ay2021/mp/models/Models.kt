9b9eb6c226fe24c39fa6d9f78baba9de0496fb11
Elijah Song <elijahs2@illinois.edu>
2022-08-02 16:49:27 -0500

    MP2 finish!!!!

--- app/src/main/kotlin/edu/illinois/cs/cs124/ay2021/mp/models/Models.kt
+++ app/src/main/kotlin/edu/illinois/cs/cs124/ay2021/mp/models/Models.kt
@@ -53,14 +53,12 @@ fun List<Restaurant>.search(input: String): List<Restaurant> {
     // Check if cuisine in the list
     if (cuisines.contains(cleanInput)) {
         // return all restaurants from that cuisine
-        println("Exact cuisine match $cleanInput")
         for (restaurant in this) {
-            if (restaurant.cuisine.equals(cleanInput)) {
+            if (restaurant.cuisine == cleanInput) {
                 toReturn += restaurant
             }
         }
     } else {
-        println("General search for $cleanInput")
         for (restaurant in this) {
             if (restaurant.name.lowercase().contains(cleanInput) || restaurant.cuisine.lowercase().contains
                 (cleanInput)
@@ -79,13 +77,42 @@ fun List<Restaurant>.search(input: String): List<Restaurant> {
  * You will need to understand some of the code in this file and make changes starting with MP2 part 1.
  *
  */
-class Preference
+class Preference(val id: String, val restaurantIDs: List<String>) {
+    constructor() : this("", listOf<String>())
+}
 
 /*
  * Related restaurants
  */
 class RelatedRestaurants(restaurants: List<Restaurant>, preferences: List<Preference>) {
+    private val restRel: MutableMap<String, MutableMap<String, Int>> = mutableMapOf()
+    init {
+        // the passed list of restaurants is the list of valid restaurants we can use; ones not in this list should
+        // not be in either map
+        val validRestaurants = mutableListOf<String>()
+        for (restaurant in restaurants) {
+            validRestaurants += restaurant.id
+        }
+        // populate restaurantRelationship; each Preference object in preferences
+        for (preference in preferences) {
+            for (r1 in preference.restaurantIDs) {
+                for (r2 in preference.restaurantIDs) {
+                    if (r1 != r2 && validRestaurants.contains(r1) && validRestaurants.contains(r2)) {
+                        var strength = restRel[r1]?.get(r2) ?: 0
+                        strength++
+                        if (restRel[r1] != null) {
+                            restRel[r1]!![r2] = strength
+                        } else {
+                            restRel[r1] = mutableMapOf(r2 to 1)
+                        }
+                    }
+                }
+            }
+        }
+    }
     fun getRelated(from: String): Map<String, Int> {
-        return mapOf()
+        print(from)
+        println(restRel[from] ?: "empty")
+        return restRel[from] ?: mapOf()
     }
 }
